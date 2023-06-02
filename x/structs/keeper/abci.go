a227a87a4994d94f6ab450f9e8379d413c15c71b
abstrct <abstrct@slow.ninja>
2023-06-02 16:10:03 -0400

    Thank you kind rakd for finding the bug in EndBlock that prevented execution of the keeper code. EndBlock is now useful again

--- x/structs/keeper/abci.go
+++ x/structs/keeper/abci.go
@@ -9,11 +9,11 @@ import (
 // BeginBlocker will persist the current header and validator set as a historical entry
 // and prune the oldest entry based on the HistoricalEntries parameter
 func (k *Keeper) BeginBlocker(ctx sdk.Context) {
-    k.UpdateSubstationStatus(sdk.UnwrapSDKContext(ctx))
+
 }
 
 // Called every block, update validator set
 func (k *Keeper) EndBlocker(ctx context.Context) ([]abci.ValidatorUpdate, error) {
-
+    k.UpdateSubstationStatus(sdk.UnwrapSDKContext(ctx))
 	return []abci.ValidatorUpdate{}, nil
 }
--- x/structs/module.go
+++ x/structs/module.go
@@ -173,10 +173,12 @@ func (AppModule) ConsensusVersion() uint64 { return 1 }
 
 // BeginBlock contains the logic that is automatically triggered at the beginning of each block
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
-    am.keeper.BeginBlocker(ctx)
+    //
 }
 
+
 // EndBlock contains the logic that is automatically triggered at the end of each block
-func (am AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) ([]abci.ValidatorUpdate, error)  {
-	return []abci.ValidatorUpdate{}, nil
+func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+    am.keeper.EndBlocker(ctx)
+    return []abci.ValidatorUpdate{}
 }
