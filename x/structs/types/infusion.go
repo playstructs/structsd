b012b83b9ce4e53a31cd770f03374a67797fc3bf
abstrct <abstrct@slow.ninja>
2024-02-09 14:23:15 -0500

    Cleaner Infusion system taking shape.

--- x/structs/types/infusion.go
+++ x/structs/types/infusion.go
@@ -1,47 +1,72 @@
 package types
 
 import (
-	//sdk "github.com/cosmos/cosmos-sdk/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"cosmossdk.io/math"
 )
 
-func (a *Infusion) SetAddress(address string) error {
+func (a *Infusion) SetCommission(newCommission sdk.Dec) (
+                                                newInfusionEnergy uint64,
+                                                oldInfusionEnergy uint64,
+                                                newCommissionEnergy uint64,
+                                                oldCommissionEnergy uint64,
+                                                newPlayerEnergy uint64,
+                                                oldPlayerEnergy uint64,
+                                                err error)  {
 
-	a.Address = address
+    oldInfusionEnergy       = a.Energy
+    oldCommissionEnergy     = a.Commission.Mul(math.LegacyNewDecFromInt(math.NewIntFromUint64(oldInfusionEnergy))).RoundInt().Uint64()
+    oldPlayerEnergy         = a.Energy - oldCommissionEnergy
 
-	return nil
+
+    newInfusionEnergy       = CalculateInfusionEnergy(a.DestinationType, a.Fuel)
+    newCommissionEnergy     = newCommission.Mul(math.LegacyNewDecFromInt(math.NewIntFromUint64(newInfusionEnergy))).RoundInt().Uint64()
+    newPlayerEnergy         = newInfusionEnergy - newCommissionEnergy
+
+
+	a.Commission  = newCommission
+	a.Energy      = newInfusionEnergy
+
+    err           = nil
+	return
+
 }
 
+func (a *Infusion) SetFuel(newFuel uint64) (
+                                    newInfusionEnergy uint64,
+                                    oldInfusionEnergy uint64,
+                                    newCommissionEnergy uint64,
+                                    oldCommissionEnergy uint64,
+                                    newPlayerEnergy uint64,
+                                    oldPlayerEnergy uint64,
+                                    err error)  {
 
-func (a *Infusion) SetFuel(newFuel uint64) error {
+    oldInfusionEnergy       = a.Energy
+    oldCommissionEnergy     = a.Commission.Mul(math.LegacyNewDecFromInt(math.NewIntFromUint64(oldInfusionEnergy))).RoundInt().Uint64()
+    oldPlayerEnergy         = a.Energy - oldCommissionEnergy
 
-	a.Fuel   = newFuel
-	a.Energy = CalculateInfusionEnergy(a.DestinationType, newFuel)
 
-	return nil
+    newInfusionEnergy       = CalculateInfusionEnergy(a.DestinationType, a.Fuel)
+    newCommissionEnergy     = a.Commission.Mul(math.LegacyNewDecFromInt(math.NewIntFromUint64(newInfusionEnergy))).RoundInt().Uint64()
+    newPlayerEnergy         = newInfusionEnergy - newCommissionEnergy
+
+
+    a.Fuel      = newFuel
+	a.Energy    = newInfusionEnergy
+
+    err         = nil
+	return
+
 }
 
+func (a *Infusion) getEnergyDistribution() (infusionEnergy uint64, commissionEnergy uint64, playerEnergy uint64) {
+        infusionEnergy       = a.Energy
+        commissionEnergy     = a.Commission.Mul(math.LegacyNewDecFromInt(math.NewIntFromUint64(infusionEnergy))).RoundInt().Uint64()
+        playerEnergy         = infusionEnergy - commissionEnergy
 
-func (a *Infusion) SetDestination(destinationId uint64) error {
-
-	a.DestinationId = destinationId
-
-	return nil
+        return
 }
-
-func (a *Infusion) SetLinkedSourceAllocation(allocationId uint64) error {
-
-	a.LinkedSourceAllocationId = allocationId
-
-	return nil
-}
-
-func (a *Infusion) SetLinkedPlayerAllocation(allocationId uint64) error {
-
-	a.LinkedPlayerAllocationId = allocationId
-
-	return nil
-}
-
 
 func CalculateInfusionEnergy(destinationType ObjectType, fuel uint64) (energy uint64) {
     switch destinationType {
@@ -54,15 +79,15 @@ func CalculateInfusionEnergy(destinationType ObjectType, fuel uint64) (energy ui
     return
 }
 
-func CreateNewInfusion(destinationType ObjectType, destinationId uint64, playerAddress string, fuel uint64) Infusion {
+func CreateNewInfusion(destinationType ObjectType, destinationId uint64, playerAddress string, playerId uint64, fuel uint64, commission sdk.Dec) Infusion {
 	return Infusion{
 		DestinationType: destinationType,
 		DestinationId: destinationId,
+		Commission: commission,
 		Fuel: fuel,
 		Energy: CalculateInfusionEnergy(destinationType, fuel),
 		Address: playerAddress,
-		LinkedSourceAllocationId: 0,
-		LinkedPlayerAllocationId: 0,
+		PlayerId: playerId,
 	}
 }
 
