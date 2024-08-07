ed3d5027173e6d347b15b4fed02f3b7a92f732b5
abstrct <abstrct@slow.ninja>
2024-08-07 11:39:31 -0400

    Updated build functionality to not load the calling player object, only the taret player.

--- x/structs/keeper/msg_server_struct_build_complete.go
+++ x/structs/keeper/msg_server_struct_build_complete.go
@@ -8,7 +8,7 @@ import (
 	"structs/x/structs/types"
 )
 
-func (k msgServer) StructBuildComplete(goCtx context.Context, msg *types.MsgStructBuildComplete) (*types.MsgStructBuildCompleteResponse, error) {
+func (k msgServer) StructBuildComplete(goCtx context.Context, msg *types.MsgStructBuildComplete) (*types.MsgStructStatusResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
     // Add an Active Address record to the
@@ -17,44 +17,42 @@ func (k msgServer) StructBuildComplete(goCtx context.Context, msg *types.MsgStru
 
     callingPlayerIndex := k.GetPlayerIndexFromAddress(ctx, msg.Creator)
     if (callingPlayerIndex == 0) {
-        return &types.MsgStructBuildCompleteResponse{}, sdkerrors.Wrapf(types.ErrPlayerRequired, "Struct build actions requires Player account but none associated with %s", msg.Creator)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrPlayerRequired, "Struct build actions requires Player account but none associated with %s", msg.Creator)
     }
-    callingPlayer, _ := k.GetPlayerFromIndex(ctx, callingPlayerIndex, true)
+    //callingPlayer, _ := k.GetPlayerFromIndex(ctx, callingPlayerIndex, true)
+    callingPlayerId := GetObjectID(types.ObjectType_player, callingPlayerIndex)
 
     addressPermissionId := GetAddressPermissionIDBytes(msg.Creator)
     // Make sure the address calling this has Play permissions
     if (!k.PermissionHasOneOf(ctx, addressPermissionId, types.PermissionPlay)) {
-        return &types.MsgStructBuildCompleteResponse{}, sdkerrors.Wrapf(types.ErrPermissionPlay, "Calling address (%s) has no play permissions ", msg.Creator)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrPermissionPlay, "Calling address (%s) has no play permissions ", msg.Creator)
     }
 
     structure, structureFound := k.GetStruct(ctx, msg.StructId)
     if (!structureFound) {
-        return &types.MsgStructBuildCompleteResponse{}, sdkerrors.Wrapf(types.ErrObjectNotFound, "Struct (%s) not found", msg.StructId)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrObjectNotFound, "Struct (%s) not found", msg.StructId)
     }
 
     if (callingPlayer.Id != structure.Owner) {
         // Check permissions on Creator on Planet
-        playerPermissionId := GetObjectPermissionIDBytes(structure.Owner, callingPlayer.Id),
+        playerPermissionId := GetObjectPermissionIDBytes(structure.Owner, callingPlayerId)
         if (!k.PermissionHasOneOf(ctx, playerPermissionId, types.PermissionPlay)) {
-            return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrPermissionPlay, "Calling account (%s) has no play permissions on target player (%s)", callingPlayer.Id, structure.Owner)
+            return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrPermissionPlay, "Calling account (%s) has no play permissions on target player (%s)", callingPlayerId, structure.Owner)
         }
-
-        sudoPlayer, _ := k.GetPlayer(ctx, structure.Owner, true)
-    } else {
-        sudoPlayer := callingPlayer
     }
+    sudoPlayer, _ := k.GetPlayer(ctx, structure.Owner, true)
 
     // Load Struct Type
     structType, structTypeFound := k.GetStructType(ctx, structure.Type)
     if (!structTypeFound) {
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrObjectNotFound, "Struct Type (%d) was not found. Building a Struct with schematics might be tough", structure.Type)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrObjectNotFound, "Struct Type (%d) was not found. Building a Struct with schematics might be tough", structure.Type)
     }
 
     // Check Sudo Player Charge
     playerCharge := k.GetPlayerCharge(ctx, structure.Owner)
     if (playerCharge < structType.ActivateCharge) {
         k.DischargePlayer(ctx, structure.Owner)
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrInsufficientCharge, "Struct Type (%d) required a charge of %d to activate, but player (%s) only had %d", msg.StructTypeId, structType.ActivateCharge, structure.Owner, playerCharge)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrInsufficientCharge, "Struct Type (%d) required a charge of %d to activate, but player (%s) only had %d", msg.StructTypeId, structType.ActivateCharge, structure.Owner, playerCharge)
     }
 
     // Check player Load for the passiveDraw capacity
@@ -67,12 +65,12 @@ func (k msgServer) StructBuildComplete(goCtx context.Context, msg *types.MsgStru
     // Is load complete shot already?
     if (sudoPlayerTotalLoad > sudoPlayerTotalCapacity) {
         k.DischargePlayer(ctx, sudoPlayer.Id)
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrGridMalfunction, "Struct Type (%d) required a draw of %d during activation, but player (%s) has none available", msg.StructTypeId, structType.BuildDraw, sudoPlayer.Id)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrGridMalfunction, "Struct Type (%d) required a draw of %d during activation, but player (%s) has none available", msg.StructTypeId, structType.BuildDraw, sudoPlayer.Id)
 
     // Otherwise is the difference enough to support the buildDraw rate
     } else if ((sudoPlayerTotalCapacity - sudoPlayerTotalLoad) < structType.PassiveDraw) {
         k.DischargePlayer(ctx, sudoPlayer.Id)
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrGridMalfunction, "Struct Type (%d) required a draw of %d during activation, but player (%s) has %d available", msg.StructTypeId, structType.BuildDraw, sudoPlayer.Id,(sudoPlayerTotalCapacity - sudoPlayerTotalLoad))
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrGridMalfunction, "Struct Type (%d) required a draw of %d during activation, but player (%s) has %d available", msg.StructTypeId, structType.BuildDraw, sudoPlayer.Id,(sudoPlayerTotalCapacity - sudoPlayerTotalLoad))
     }
 
     /* More garbage clown code rushed to make the testnet more interesting */
@@ -84,12 +82,12 @@ func (k msgServer) StructBuildComplete(goCtx context.Context, msg *types.MsgStru
 
 
     if (!types.HashBuildAndCheckBuildDifficulty(hashInput, msg.Proof, currentAge)) {
-       return &types.MsgStructBuildCompleteResponse{}, sdkerrors.Wrapf(types.ErrStructBuildComplete, "Work failure for input (%s) when trying to build Struct %s", hashInput, structure.Id)
+       return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrStructBuildComplete, "Work failure for input (%s) when trying to build Struct %s", hashInput, structure.Id)
     }
 
     // Try to bring online if there is room in the energy cap
     if (!player.CanSupportNewLoad(structure.PassiveDraw)) {
-        return &types.MsgStructBuildCompleteResponse{}, sdkerrors.Wrapf(types.ErrStructBuildComplete, "Could not bring Struct %s online, player %s does not have enough power",structure.Id, player.Id)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrStructBuildComplete, "Could not bring Struct %s online, player %s does not have enough power",structure.Id, player.Id)
     }
 
     // TODO fix because buildDraw needs to be removed
@@ -103,5 +101,5 @@ func (k msgServer) StructBuildComplete(goCtx context.Context, msg *types.MsgStru
     //k.SetStruct(ctx, structure)
 
 
-	return &types.MsgStructBuildCompleteResponse{Struct: structure}, nil
+	return &types.MsgStructStatusResponse{Struct: structure}, nil
 }
--- x/structs/keeper/msg_server_struct_build_initiate.go
+++ x/structs/keeper/msg_server_struct_build_initiate.go
@@ -15,7 +15,7 @@ import (
   uint64 slot           = 5;
   */
 
-func (k msgServer) StructBuildInitiate(goCtx context.Context, msg *types.MsgStructBuildInitiate) (*types.MsgStructBuildInitiateResponse, error) {
+func (k msgServer) StructBuildInitiate(goCtx context.Context, msg *types.MsgStructBuildInitiate) (*types.MsgStructStatusResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
     // Add an Active Address record to the
@@ -25,28 +25,29 @@ func (k msgServer) StructBuildInitiate(goCtx context.Context, msg *types.MsgStru
     // Load Initiator Player from the Creator
     callingPlayerIndex := k.GetPlayerIndexFromAddress(ctx, msg.Creator)
     if (callingPlayerIndex == 0) {
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrPlayerRequired, "Struct build initialization requires Player account but none associated with %s", msg.Creator)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrPlayerRequired, "Struct build initialization requires Player account but none associated with %s", msg.Creator)
     }
-    callingPlayer, _ := k.GetPlayerFromIndex(ctx, callingPlayerIndex, true)
+    callingPlayerId := GetObjectID(types.ObjectType_player, callingPlayerIndex)
+    //callingPlayer, _ := k.GetPlayerFromIndex(ctx, callingPlayerIndex, true)
 
 
     // Load Planet from the PlanetId
     planet, planetFound := k.GetPlanet(ctx, msg.PlanetId)
     if (!planetFound) {
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrObjectNotFound, "Planet (%s) was ot found. Building a Struct in a void might be tough", msg.PlanetId)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrObjectNotFound, "Planet (%s) was ot found. Building a Struct in a void might be tough", msg.PlanetId)
     }
 
     sudoPlayerIndex := k.GetPlayerIndexFromAddress(ctx, planet.Owner)
     if (sudoPlayerIndex == 0) {
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrPlayerRequired, "Struct build initialization requires Player but somehow planet has none %s", planet.Owner)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrPlayerRequired, "Struct build initialization requires Player but somehow planet has none %s", planet.Owner)
     }
     sudoPlayer, _ := k.GetPlayerFromIndex(ctx, sudoPlayerIndex, true)
 
-    if (sudoPlayer.Id != callingPlayer.Id) {
+    if (sudoPlayer.Id != callingPlayerId) {
         // Check permissions on Creator on Planet
-        playerPermissionId := GetObjectPermissionIDBytes(sudoPlayer.Id, callingPlayer.Id),
+        playerPermissionId := GetObjectPermissionIDBytes(sudoPlayer.Id, callingPlayerId)
         if (!k.PermissionHasOneOf(ctx, playerPermissionId, types.PermissionPlay)) {
-            return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrPermissionPlay, "Calling account (%s) has no play permissions on target player (%s)", callingPlayer.Id, sudoPlayer.Id)
+            return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrPermissionPlay, "Calling account (%s) has no play permissions on target player (%s)", callingPlayerId, sudoPlayer.Id)
         }
 
     }
@@ -54,7 +55,7 @@ func (k msgServer) StructBuildInitiate(goCtx context.Context, msg *types.MsgStru
     // Make sure the address calling this has Play permissions
     addressPermissionId := GetAddressPermissionIDBytes(msg.Creator)
     if (!k.PermissionHasOneOf(ctx, addressPermissionId, types.PermissionPlay)) {
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrPermissionPlay, "Calling address (%s) has no play permissions ", msg.Creator)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrPermissionPlay, "Calling address (%s) has no play permissions ", msg.Creator)
     }
 
     // Check Ambit / Slot
@@ -72,33 +73,33 @@ func (k msgServer) StructBuildInitiate(goCtx context.Context, msg *types.MsgStru
             planetSlots := planet.SpaceSlots
             planetSlot  := planet.Space[msg.Slot]
         default:
-            return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrStructBuildInitiate, "The Struct Build was initiated on a non-existant ambit")
+            return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrStructBuildInitiate, "The Struct Build was initiated on a non-existent ambit")
     }
 
     if (msg.Slot >= planetSlots) {
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrStructBuildInitiate, "The planet (%s) specified doesn't have that slot available to build on", msg.PlanetId)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrStructBuildInitiate, "The planet (%s) specified doesn't have that slot available to build on", msg.PlanetId)
     }
     if (planetSlot != "") {
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrStructBuildInitiate, "The planet (%s) specified already has a struct on that slot", msg.PlanetId)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrStructBuildInitiate, "The planet (%s) specified already has a struct on that slot", msg.PlanetId)
     }
 
     // Load Struct Type
     structType, structTypeFound := k.GetStructType(ctx, msg.StructTypeId)
     if (!structTypeFound) {
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrObjectNotFound, "Struct Type (%d) was not found. Building a Struct with schematics might be tough", msg.StructTypeId)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrObjectNotFound, "Struct Type (%d) was not found. Building a Struct with schematics might be tough", msg.StructTypeId)
     }
 
     // Check Sudo Player Charge
     playerCharge := k.GetPlayerCharge(ctx, sudoPlayer.Id)
     if (playerCharge < structType.BuildCharge) {
         k.DischargePlayer(ctx, sudoPlayer)
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrInsufficientCharge, "Struct Type (%d) required a charge of %d to build, but player (%s) only had %d", msg.StructTypeId, structType.BuildCharge, sudoPlayer.Id, playerCharge)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrInsufficientCharge, "Struct Type (%d) required a charge of %d to build, but player (%s) only had %d", msg.StructTypeId, structType.BuildCharge, sudoPlayer.Id, playerCharge)
     }
 
     // This process will check the location details to make sure they're acceptable based on the structType
     structure, err := types.CreateBaseStruct(structType, msg.Creator, sudoPlayer.Id, planet.Id, types.ObjectType_planet, msg.Ambit, msg.Slot)
     if (err != nil) {
-        return &types.MsgStructBuildInitiateResponse{}, err
+        return &types.MsgStructStatusResponse{}, err
     }
 
     // Check player Load for the buildDraw capacity
@@ -107,12 +108,12 @@ func (k msgServer) StructBuildInitiate(goCtx context.Context, msg *types.MsgStru
     // Is load complete shot already?
     if (sudoPlayerTotalLoad > sudoPlayerTotalCapacity) {
         k.DischargePlayer(ctx, sudoPlayer.Id)
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrGridMalfunction, "Struct Type (%d) required a draw of %d during build, but player (%s) has none available", msg.StructTypeId, structType.BuildDraw, sudoPlayer.Id)
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrGridMalfunction, "Struct Type (%d) required a draw of %d during build, but player (%s) has none available", msg.StructTypeId, structType.BuildDraw, sudoPlayer.Id)
 
     // Otherwise is the difference enough to support the buildDraw rate
     } else if ((sudoPlayerTotalCapacity - sudoPlayerTotalLoad) < structType.BuildDraw) {
         k.DischargePlayer(ctx, sudoPlayer.Id)
-        return &types.MsgStructBuildInitiateResponse{}, sdkerrors.Wrapf(types.ErrGridMalfunction, "Struct Type (%d) required a draw of %d during build, but player (%s) has %d available", msg.StructTypeId, structType.BuildDraw, sudoPlayer.Id,(sudoPlayerTotalCapacity - sudoPlayerTotalLoad))
+        return &types.MsgStructStatusResponse{}, sdkerrors.Wrapf(types.ErrGridMalfunction, "Struct Type (%d) required a draw of %d during build, but player (%s) has %d available", msg.StructTypeId, structType.BuildDraw, sudoPlayer.Id,(sudoPlayerTotalCapacity - sudoPlayerTotalLoad))
     }
 
     // Increase the Struct load of the sudoPlayer
@@ -129,9 +130,9 @@ func (k msgServer) StructBuildInitiate(goCtx context.Context, msg *types.MsgStru
     if (err != nil) {
         // This is a pretty huge problem if we get here since all the other crap is done.
         // Roll back transaction?
-        return &types.MsgStructBuildInitiateResponse{}, err
+        return &types.MsgStructStatusResponse{}, err
     }
     k.SetPlanet(ctx, planet)
 
-	return &types.MsgStructBuildInitiateResponse{Struct: structure}, nil
+	return &types.MsgStructStatusResponse{Struct: structure}, nil
 }
