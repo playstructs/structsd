b4ee4f93ccf08e26394a101299604d84106bc1d9
abstrct <abstrct@slow.ninja>
2024-08-28 15:36:47 -0400

    Small formatting fixes to event attack details

--- x/structs/types/event_attack_detail.go
+++ x/structs/types/event_attack_detail.go
@@ -6,9 +6,6 @@ import (
 )
 
 
-func (eventAttackDetail *EventAttackDetail) SetSomething() {
-}
-
 func CreateEventAttackDetail() (*EventAttackDetail) {
     return &EventAttackDetail{}
 }
@@ -32,6 +29,7 @@ func (eventAttackDetail *EventAttackDetail) AppendShot(eventAttackShotDetail *Ev
 }
 
 
+/* Sub Event - Attack Shots */
 func CreateEventAttackShotDetail(targetStructId string) (*EventAttackShotDetail) {
     return &EventAttackShotDetail{
         TargetStructId: targetStructId,
@@ -64,16 +62,12 @@ func (eventAttackShotDetail *EventAttackShotDetail) SetDamage(damage uint64) {
     eventAttackShotDetail.Damage = damage
 }
 
-
 func (eventAttackShotDetail *EventAttackShotDetail) SetPostDestructionDamage(postDestructionDamage uint64, postDestructionDamageDestroyedAttacker bool, postDestructionDamageCause TechPassiveWeaponry) {
-  eventAttackShotDetail.PostDestructionDamageToAttacker         = true
-  eventAttackShotDetail.PostDestructionDamage                   = postDestructionDamage
-  eventAttackShotDetail.PostDestructionDamageDestroyedAttacker  = postDestructionDamageDestroyedAttacker
-  eventAttackShotDetail.PostDestructionDamageCause              = postDestructionDamageCause
+    eventAttackShotDetail.PostDestructionDamageToAttacker         = true
+    eventAttackShotDetail.PostDestructionDamage                   = postDestructionDamage
+    eventAttackShotDetail.PostDestructionDamageDestroyedAttacker  = postDestructionDamageDestroyedAttacker
+    eventAttackShotDetail.PostDestructionDamageCause              = postDestructionDamageCause
 }
-
-
-
 
 func (eventAttackShotDetail *EventAttackShotDetail) AppendDefenderCounter(counterByStructId string, counterDamage uint64, counterDestroyedAttacker bool) {
     eventAttackDefenderCounterDetail := EventAttackDefenderCounterDetail{
@@ -92,43 +86,3 @@ func (eventAttackShotDetail *EventAttackShotDetail) AppendTargetCounter(counterD
     eventAttackShotDetail.TargetCounterCause = passiveWeaponry
 }
 
-
-/*
-message EventAttackShotDetail {
-  string  targetStructId = 1;
-
-  bool evaded                  = 2;
-  techUnitDefenses evadedCause = 3;
-
-  bool blocked              = 4;
-  string blockedByStructId  = 5;
-
-  repeated EventAttackDefenderCounterDetail eventAttackDefenderCounterDetail = 6;
-
-  uint64 damageDealt                    = 7;
-  uint64 damageReduction                = 8;
-  techUnitDefenses damageReductionCause = 9;
-  uint64 damage                         = 10;
-
-
-  bool targetCountered                    = 11;
-  uint64 targetCounteredDamage            = 12;
-  bool targetCounterDestroyedAttacker     = 13;
-  techPassiveWeaponry targetCounterCause  = 14;
-
-  bool targetDestroyed = 15;
-
-  bool postDestructionDamageToAttacker            = 16;
-  uint64 postDestructionDamage                    = 17;
-  bool postDestructionDamageDestroyedAttacker     = 18;
-  techPassiveWeaponry postDestructionDamageCause  = 19;
-
-}
-
-message EventAttackDefenderCounterDetail {
-  string  counterByStructId         = 1;
-  uint64  counterDamage             = 2;
-  bool    counterDestroyedAttacker  = 3;
-}
-
-*/
