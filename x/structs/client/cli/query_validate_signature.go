5ab26df5991e9c47b24a9e7dffb78599453f8037
abstrct <abstrct@slow.ninja>
2024-10-03 13:50:58 -0400

    Created a validate-signature query for external services to use to easily validate things. Initial use is for session validation at the API layer.

---
+++ x/structs/client/cli/query_validate_signature.go
@@ -0,0 +1,55 @@
+package cli
+
+import (
+
+    "context"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+	"structs/x/structs/types"
+
+)
+/*
+message QueryValidateSignatureRequest {
+  string address        = 1;
+  string message        = 2;
+  string proofPubKey    = 3;
+  string proofSignature = 4;
+}
+
+message QueryValidateSignatureResponse {
+  bool pubkeyFormatError      = 1;
+  bool signatureFormatError   = 2;
+  bool addressPubkeyMismatch  = 3;
+  bool signatureInvalid       = 4;
+  bool valid                  = 5;
+}
+*/
+func CmdPlayerMe() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "validate-signature [address] [message] [signature] [pubkey]",
+		Short: "Validate the signature and source of a generic message",
+		Args:  cobra.ExactArgs(4),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+            params := types.QueryValidateSignatureRequest{
+                  Address:          args[0],
+                  Message:          args[1],
+                  ProofPubKey:      args[2],
+                  ProofSignature:   args[3],
+            }
+
+			res, err := queryClient.ValidateSignature(cmd.Context(), &params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+	return cmd
+}
