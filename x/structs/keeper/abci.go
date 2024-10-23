bfb7bc4dcb9034c703475330d60806a23c7e66d9
abstrct <abstrct@slow.ninja>
2024-10-22 22:56:52 -0400

    Added some weird code that will emit all the objects and attributes created during genesis. This makes the indexer better.

--- x/structs/keeper/abci.go
+++ x/structs/keeper/abci.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	abci "github.com/cometbft/cometbft/abci/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"structs/x/structs/types"
 )
 
 // BeginBlocker will persist the current header and validator set as a historical entry
 // and prune the oldest entry based on the HistoricalEntries parameter
 func (k *Keeper) BeginBlocker(ctx context.Context) {
+
+    k.EventAllGenesis(ctx)
+
     k.StructSweepDestroyed(ctx)
 }
 
@@ -23,4 +27,56 @@ func (k *Keeper) EndBlocker(ctx context.Context) ([]abci.ValidatorUpdate, error)
 	k.GridCascade(sdk.UnwrapSDKContext(ctx))
 
 	return []abci.ValidatorUpdate{}, nil
+}
+
+func (k *Keeper) EventAllGenesis(ctx context.Context) {
+    ctxSDK := sdk.UnwrapSDKContext(ctx)
+
+    if ctxSDK.BlockHeight() > 1 { return }
+
+	// Player
+    players := k.GetAllPlayer(ctx)
+    for _, player := range players {
+        _ = ctxSDK.EventManager().EmitTypedEvent(&types.EventPlayer{Player: &player})
+    }
+
+	// Address
+    addresses := k.GetAllAddressExport(ctx)
+    for _, address := range addresses {
+        _ = ctxSDK.EventManager().EmitTypedEvent(&types.EventAddressAssociation{&types.AddressAssociation{Address: address.Address, PlayerIndex: address.PlayerIndex, RegistrationStatus: types.RegistrationStatus_approved}})
+	}
+
+	// Permissions
+    permissions := k.GetAllPermissionExport(ctx)
+    for _, permission := range permissions {
+        _ = ctxSDK.EventManager().EmitTypedEvent(&types.EventPermission{&types.PermissionRecord{PermissionId: permission.PermissionId, Value: permission.Value}})
+    }
+
+	// Grid Attributes
+	grids := k.GetAllGridExport(ctx)
+    for _, grid := range grids {
+        _ = ctxSDK.EventManager().EmitTypedEvent(&types.EventGrid{&types.GridRecord{AttributeId: grid.AttributeId, Value: grid.Value}})
+    }
+
+	// Reactor
+    reactors := k.GetAllReactor(ctx)
+    for _, reactor := range reactors {
+        _ = ctxSDK.EventManager().EmitTypedEvent(&types.EventReactor{Reactor: &reactor})
+    }
+
+	// Infusion
+    infusions := k.GetAllInfusion(ctx)
+    for _, infusion := range infusions {
+        _ = ctxSDK.EventManager().EmitTypedEvent(&types.EventInfusion{Infusion: &infusion})
+    }
+
+	// Struct Type
+    structTypes := k.GetAllStructType(ctx)
+    for _, structType := range structTypes {
+        _ = ctxSDK.EventManager().EmitTypedEvent(&types.EventStructType{StructType: &structType})
+    }
+
+	// Banking
+
+
 }
