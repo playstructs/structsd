c85a71c78f3bab61694b1b8f4379cdbf13627451
abstrct <abstrct@slow.ninja>
2025-02-21 23:53:06 -0500

    Basic MsgServer functions

---
+++ x/structs/keeper/msg_server_provider_guild_grant.go
@@ -0,0 +1,19 @@
+package keeper
+
+import (
+	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	//sdkerrors "cosmossdk.io/errors"
+	"structs/x/structs/types"
+)
+
+func (k msgServer) ProviderGuildGrant(goCtx context.Context, msg *types.MsgProviderGuildGrant) (*types.MsgProviderResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+    // Add an Active Address record to the
+    // indexer for UI requirements
+	k.AddressEmitActivity(ctx, msg.Creator)
+
+
+	return &types.MsgProviderResponse{}, nil
+}
