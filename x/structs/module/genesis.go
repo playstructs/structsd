2acf3552753f53f5a231dd461fd82161fc8f3997
abstrct <abstrct@slow.ninja>
2026-01-29 10:35:40 -0500

    Fixing future exports to include a non-terrible blockstart

--- x/structs/module/genesis.go
+++ x/structs/module/genesis.go
@@ -93,11 +93,10 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
         k.SetStruct(ctx, elem)
     }
     // Struct attributes
-    // TODO Update block based values to 0
     for _, elem := range genState.StructAttributeList {
         value := elem.Value
         if isStructBlockHeightAttribute(elem.AttributeId) {
-            value = 0
+            value = 1
         }
         k.SetStructAttribute(ctx, elem.AttributeId, value)
     }
