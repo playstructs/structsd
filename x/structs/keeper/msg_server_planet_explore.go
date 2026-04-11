964460cb8605ec15c380e8a95d00db90b2eae1b1
abstrct <abstrct@slow.ninja>
2026-04-11 15:42:41 -0400

    Added a new fleet is on station requirement for planet exploration.

--- x/structs/keeper/msg_server_planet_explore.go
+++ x/structs/keeper/msg_server_planet_explore.go
@@ -2,58 +2,64 @@ package keeper
 
 import (
 	"context"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"structs/x/structs/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) PlanetExplore(goCtx context.Context, msg *types.MsgPlanetExplore) (*types.MsgPlanetExploreResponse, error) {
-    emptyResponse := &types.MsgPlanetExploreResponse{}
+	emptyResponse := &types.MsgPlanetExploreResponse{}
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	cc := k.NewCurrentContext(ctx)
 
-    // Add an Active Address record to the
-    // indexer for UI requirements
+	// Add an Active Address record to the
+	// indexer for UI requirements
 	k.AddressEmitActivity(ctx, msg.Creator)
 
-    callingPlayer, err := cc.GetPlayerByAddress(msg.Creator)
-    if err != nil {
-        return emptyResponse, err
-    }
+	callingPlayer, err := cc.GetPlayerByAddress(msg.Creator)
+	if err != nil {
+		return emptyResponse, err
+	}
 
-    // Load the Player record
-    player, playerLookupErr := cc.GetPlayer(msg.PlayerId)
-    if (playerLookupErr != nil) {
-        return emptyResponse, playerLookupErr
-    }
+	// Load the Player record
+	player, playerLookupErr := cc.GetPlayer(msg.PlayerId)
+	if playerLookupErr != nil {
+		return emptyResponse, playerLookupErr
+	}
 
-    // Check address play permissions
-    permissionError := player.CanBePlayedBy(callingPlayer)
-    if (permissionError != nil) {
-        return emptyResponse, permissionError
-    }
+	// Check address play permissions
+	permissionError := player.CanBePlayedBy(callingPlayer)
+	if permissionError != nil {
+		return emptyResponse, permissionError
+	}
 
-    // Is the Player online?
-    readinessError := player.ReadinessCheck()
-    if (readinessError != nil) {
-        return emptyResponse, readinessError
-    }
+	// Is the Player online?
+	readinessError := player.ReadinessCheck()
+	if readinessError != nil {
+		return emptyResponse, readinessError
+	}
 
-    // check if there is a planet currently
-        // check that the planet can be completed
-        // complete the previous planet
-    if (player.HasPlanet()){
-        planetCompletionError := player.GetPlanet().AttemptComplete()
-        if (planetCompletionError != nil) {
-            return emptyResponse, planetCompletionError
-        }
-    }
+	// check if there is a planet currently
+	// check that the planet can be completed
+	// complete the previous planet
+	if player.HasPlanet() {
+		// Is the Fleet on station?
+		if player.GetFleet().IsAway() {
+			return emptyResponse, types.NewFleetStateError(player.GetFleetId(), "away", "explore")
+		}
 
-    planetExploreError := player.AttemptPlanetExplore()
-    if (planetExploreError != nil) {
-        return emptyResponse, planetExploreError
-    }
+		planetCompletionError := player.GetPlanet().AttemptComplete()
+		if planetCompletionError != nil {
+			return emptyResponse, planetCompletionError
+		}
+	}
 
-    player.GetFleet().MigrateToNewPlanet(player.GetPlanet())
+	planetExploreError := player.AttemptPlanetExplore()
+	if planetExploreError != nil {
+		return emptyResponse, planetExploreError
+	}
+
+	player.GetFleet().MigrateToNewPlanet(player.GetPlanet())
 
 	cc.CommitAll()
 	return &types.MsgPlanetExploreResponse{Planet: player.GetPlanet().GetPlanet()}, nil
